529d3304f5aa3eacf0c91411163d9e02e6bbffb5
LYS <lys219911@gmail.com>
2022-11-27 22:18:00 +0800

    feat: 测试登录提交

--- src/main.go
+++ src/main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"html/template"
 )
 
-func main () {
-	fmt.Println("Hello World")
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Println("val: ", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatal("ParseForm: ", err)
+		}
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
